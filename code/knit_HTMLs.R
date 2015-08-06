2bf4bcdc61ae50b72bb6ff2a87dc83422fbb2ba0
amsantac <amsantac@gmail.com>
2015-08-05 19:49:55 -0500

    update and organize html and md files
    - updating html files using GitHub pages generator was deprecated
    - md files were moved from the master branch to the gh-pages branch
    - iframes for maps and tables were added to the md files
    - knit_HTMLs.R file was created and added to code folder. This R script
    helps for automated updating of html files using the md files
    - the header and footer sections of the html files were separated in two
    distinct files, stored in www

---
+++ code/knit_HTMLs.R
@@ -0,0 +1,27 @@
+library(knitr)
+
+setwd("C:/amsantac/other/temp/borrar/cuproject")
+
+## files to be processed
+files <- c("index", "phase1")
+header <- "www/header.html"
+footer <- "www/footer.html"
+
+for (file_i in files){ 
+  
+  ## read and knit md file
+  mdFile <- paste0("md/", file_i, ".md")
+  htmlFile <- paste0(file_i, ".html")
+  knit2html(mdFile, htmlFile)
+  
+  ## read temporary html file
+  htmlLines <- readLines(htmlFile)
+  
+  ## replace header and footer in temporary html file
+  suppressWarnings(newHTML <- c(readLines(header), htmlLines[(which(htmlLines == "<body>") + 1) : 
+                                                             (which(htmlLines == "</body>") - 1)], readLines(footer)))
+  
+  ## write output html file
+  writeLines(newHTML, htmlFile)
+  if(file.exists(paste0(file_i, ".txt"))) file.remove(paste0(file_i, ".txt"))
+}

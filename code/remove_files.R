e483a0972fb018ba6e298fac3ed483e3ed0653a2
amsantac <amsantac@gmail.com>
2016-01-10 15:04:29 -0500

    deleted code files

--- code/remove_files.R
+++
@@ -1,7 +0,0 @@
-## Use with caution! Files are NOT moved to the Recycle Bin
-
-## delete _refl files in folder for year 2000
-fPath <- "C:/amsantac/PhD/Research/dissertation/data/landsat/images/2000"
-list.files(fPath, pattern = "_refl", recursive = TRUE)
-file.remove(paste0(fPath, "/", list.files(fPath, pattern = "_refl", recursive = TRUE)))
-
--- code/sampling_cobertura_02.R
+++
@@ -1,7 +0,0 @@
-##source("shared/dissertation/code/sampling_cobertura_02.R")
-library(rgrass7)
-#ecosistemas <- readVECT("ecosistemas_llanos_latlon_v2_clip_757")
-  
-sizes <- read.csv("shared/dissertation/processing/other/sampling_sizes.csv")
-
-
